d25458818eb18506a4a2e8f76bc9df57b9151651
Jorge Luis <jorgefloriano1646@gmail.com>
2024-02-22 11:37:01 -0300

    first

---
+++ database/migrations/2024_02_22_135923_create_task_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('description', 250);
+            $table->tinyInteger('visible', false, true,)->default(1);
+            $table->dateTime('done')->nullable();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->dateTime('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
